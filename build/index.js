a1ff0f434b4c19caf29d91ece233c84868a3e62f
spaceshark123 <amogh.patnaik@icloud.com>
2025-01-04 13:41:44 -0600

    add more endpoints for testing parameters and queries

--- build/index.js
+++ build/index.js
@@ -9,6 +9,25 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.get('/json', (req, res) => {
+    res.json({ message: 'Hello World!' });
+});
+app.get('/double/:number', (req, res) => {
+    const number = Number(req.params.number);
+    res.json({ result: number * 2 });
+});
+app.get('/average', (req, res) => {
+    const n1 = Number(req.query.n1);
+    const n2 = Number(req.query.n2);
+    const n3 = Number(req.query.n3);
+    res.json({ result: (n1 + n2 + n3) / 3 });
+});
+app.get('/sum/:n1/:n2/:n3', (req, res) => {
+    const n1 = Number(req.params.n1);
+    const n2 = Number(req.params.n2);
+    const n3 = Number(req.params.n3);
+    res.json({ result: n1 + n2 + n3 });
+});
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,29 @@ app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
+app.get('/json', (req, res) => { // http://localhost:3000/json
+	res.json({ message: 'Hello World!' });
+});
+
+app.get('/double/:number', (req, res) => { // http://localhost:3000/double/5
+	const number = Number(req.params.number);
+	res.json({ result: number * 2 });
+});
+
+app.get('/average', (req, res) => { // http://localhost:3000/average?n1=1&n2=2&n3=3
+	const n1 = Number(req.query.n1);
+	const n2 = Number(req.query.n2);
+	const n3 = Number(req.query.n3);
+	res.json({ result: (n1 + n2 + n3) / 3 });
+});
+
+app.get('/sum/:n1/:n2/:n3', (req, res) => { // http://localhost:3000/sum/1/2/3
+	const n1 = Number(req.params.n1);
+	const n2 = Number(req.params.n2);
+	const n3 = Number(req.params.n3);
+	res.json({ result: n1 + n2 + n3 });
+});
+
 app.listen(port, () => {
 	console.log(`Server started at http://localhost:${port}`);
 });
